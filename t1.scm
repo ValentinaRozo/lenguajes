2ff627681229b37c665c662a826278adda79ee4f
Valentina Rozo Bernal <69249992+ValentinaRozo@users.noreply.github.com>
2021-05-24 13:17:38 -0500

    Add files via upload

---
+++ t1.scm
@@ -0,0 +1,200 @@
+(define pokemones (list (list "Bulbasaur" (list  "Grass" "Poison") (list  "Fire" "Ice" "Flying" "Psychic"))
+(list "Ivysaur" (list  "Grass" "Poison") (list  "Fire" "Ice" "Flying" "Psychic"))
+(list "Venusaur" (list  "Grass" "Poison") (list  "Fire" "Ice" "Flying" "Psychic"))
+(list "Charmander" (list  "Fire") (list  "Water" "Ground" "Rock"))
+(list "Charmeleon" (list  "Fire") (list  "Water" "Ground" "Rock"))
+(list "Charizard" (list  "Fire" "Flying") (list  "Water" "Electric" "Rock"))
+(list "Squirtle" (list  "Water") (list  "Electric" "Grass"))
+(list "Wartortle" (list  "Water") (list  "Electric" "Grass"))
+(list "Blastoise" (list  "Water") (list  "Electric" "Grass"))
+(list "Caterpie" (list  "Bug") (list  "Fire" "Flying" "Rock"))
+(list "Metapod" (list  "Bug") (list  "Fire" "Flying" "Rock"))
+(list "Butterfree" (list  "Bug" "Flying") (list  "Fire" "Electric" "Ice" "Flying" "Rock"))
+(list "Weedle" (list  "Bug" "Poison") (list  "Fire" "Flying" "Psychic" "Rock"))
+(list "Kakuna" (list  "Bug" "Poison") (list  "Fire" "Flying" "Psychic" "Rock"))
+(list "Beedrill" (list  "Bug" "Poison") (list  "Fire" "Flying" "Psychic" "Rock"))
+(list "Pidgey" (list  "Normal" "Flying") (list  "Electric" "Rock"))
+(list "Pidgeotto" (list  "Normal" "Flying") (list  "Electric" "Rock"))
+(list "Pidgeot" (list  "Normal" "Flying") (list  "Electric" "Rock"))
+(list "Rattata" (list  "Normal") (list  "Fighting"))
+(list "Raticate" (list  "Normal") (list  "Fighting"))
+(list "Spearow" (list  "Normal" "Flying") (list  "Electric" "Rock"))
+(list "Fearow" (list  "Normal" "Flying") (list  "Electric" "Rock"))
+(list "Ekans" (list  "Poison") (list  "Ground" "Psychic"))
+(list "Arbok" (list  "Poison") (list  "Ground" "Psychic"))
+(list "Pikachu" (list  "Electric") (list  "Ground"))
+(list "Raichu" (list  "Electric") (list  "Ground"))
+(list "Sandshrew" (list  "Ground") (list  "Water" "Grass" "Ice"))
+(list "Sandslash" (list  "Ground") (list  "Water" "Grass" "Ice"))
+(list "Nidoran ♀ (Female)" (list  "Poison") (list  "Ground" "Psychic"))
+(list "Nidorina" (list  "Poison") (list  "Ground" "Psychic"))
+(list "Nidoqueen" (list  "Poison" "Ground") (list  "Water" "Ice" "Ground" "Psychic"))
+(list "Nidoran ♂ (Male)" (list  "Poison") (list  "Ground" "Psychic"))
+(list "Nidorino" (list  "Poison") (list  "Ground" "Psychic"))
+(list "Nidoking" (list  "Poison" "Ground") (list  "Water" "Ice" "Ground" "Psychic"))
+(list "Clefairy" (list  "Normal") (list  "Fighting"))
+(list "Clefable" (list  "Normal") (list  "Fighting"))
+(list "Vulpix" (list  "Fire") (list  "Water" "Ground" "Rock"))
+(list "Ninetales" (list  "Fire") (list  "Water" "Ground" "Rock"))
+(list "Jigglypuff" (list  "Normal") (list  "Fighting"))
+(list "Wigglytuff" (list  "Normal") (list  "Fighting"))
+(list "Zubat" (list  "Poison" "Flying") (list  "Electric" "Ice" "Psychic" "Rock"))
+(list "Golbat" (list  "Poison" "Flying") (list  "Electric" "Ice" "Psychic" "Rock"))
+(list "Oddish" (list  "Grass" "Poison") (list  "Fire" "Ice" "Flying" "Psychic"))
+(list "Gloom" (list  "Grass" "Poison") (list  "Fire" "Ice" "Flying" "Psychic"))
+(list "Vileplume" (list  "Grass" "Poison") (list  "Fire" "Ice" "Flying" "Psychic"))
+(list "Paras" (list  "Bug" "Grass") (list  "Fire" "Ice" "Poison" "Flying" "Bug" "Rock"))
+(list "Parasect" (list  "Bug" "Grass") (list  "Fire" "Ice" "Poison" "Flying" "Bug" "Rock"))
+(list "Venonat" (list  "Bug" "Poison") (list  "Fire" "Flying" "Psychic" "Rock"))
+(list "Venomoth" (list  "Bug" "Poison") (list  "Fire" "Flying" "Psychic" "Rock"))
+(list "Diglett" (list  "Ground") (list  "Water" "Grass" "Ice"))
+(list "Dugtrio" (list  "Ground") (list  "Water" "Grass" "Ice"))
+(list "Meowth" (list  "Normal") (list  "Fighting"))
+(list "Persian" (list  "Normal") (list  "Fighting"))
+(list "Psyduck" (list  "Water") (list  "Electric" "Grass"))
+(list "Golduck" (list  "Water") (list  "Electric" "Grass"))
+(list "Mankey" (list  "Fighting") (list  "Flying" "Psychic" "Fairy"))
+(list "Primeape" (list  "Fighting") (list  "Flying" "Psychic" "Fairy"))
+(list "Growlithe" (list  "Fire") (list  "Water" "Ground" "Rock"))
+(list "Arcanine" (list  "Fire") (list  "Water" "Ground" "Rock"))
+(list "Poliwag" (list  "Water") (list  "Electric" "Grass"))
+(list "Poliwhirl" (list  "Water") (list  "Electric" "Grass"))
+(list "Poliwrath" (list  "Water" "Fighting") (list  "Electric" "Grass" "Flying" "Psychic" "Fairy"))
+(list "Abra" (list  "Psychic") (list  "Bug" "Ghost" "Dark"))
+(list "Kadabra" (list  "Psychic") (list  "Bug" "Ghost" "Dark"))
+(list "Alakazam" (list  "Psychic") (list  "Bug" "Ghost" "Dark"))
+(list "Machop" (list  "Fighting") (list  "Flying" "Psychic" "Fairy"))
+(list "Machoke" (list  "Fighting") (list  "Flying" "Psychic" "Fairy"))
+(list "Machamp" (list  "Fighting") (list  "Flying" "Psychic" "Fairy"))
+(list "Bellsprout" (list  "Grass" "Poison") (list  "Fire" "Ice" "Flying" "Psychic"))
+(list "Weepinbell" (list  "Grass" "Poison") (list  "Fire" "Ice" "Flying" "Psychic"))
+(list "Victreebel" (list  "Grass" "Poison") (list  "Fire" "Ice" "Flying" "Psychic"))
+(list "Tentacool" (list  "Water" "Poison") (list  "Electric" "Ground" "Psychic"))
+(list "Tentacruel" (list  "Water" "Poison") (list  "Electric" "Ground" "Psychic"))
+(list "Geodude" (list  "Rock" "Ground") (list  "Water" "Grass" "Ice" "Fighting" "Ground" "Steel"))
+(list "Graveler" (list  "Rock" "Ground") (list  "Water" "Grass" "Ice" "Fighting" "Ground" "Steel"))
+(list "Golem" (list  "Rock" "Ground") (list  "Water" "Grass" "Ice" "Fighting" "Ground" "Steel"))
+(list "Ponyta" (list  "Fire") (list  "Water" "Ground" "Rock"))
+(list "Rapidash" (list  "Fire") (list  "Water" "Ground" "Rock"))
+(list "Slowpoke" (list  "Water" "Psychic") (list  "Electric" "Grass" "Bug" "Ghost" "Dark"))
+(list "Slowbro" (list  "Water" "Psychic") (list  "Electric" "Grass" "Bug" "Ghost" "Dark"))
+(list "Magnemite" (list  "Electric") (list  "Fire" "Water" "Ground"))
+(list "Magneton" (list  "Electric") (list  "Fire" "Water" "Ground"))
+(list "Farfetch'd" (list  "Normal" "Flying") (list  "Electric" "Rock"))
+(list "Doduo" (list  "Normal" "Flying") (list  "Electric" "Rock"))
+(list "Dodrio" (list  "Normal" "Flying") (list  "Electric" "Rock"))
+(list "Seel" (list  "Water") (list  "Electric" "Grass"))
+(list "Dewgong" (list  "Water" "Ice") (list  "Electric" "Grass" "Fighting" "Rock"))
+(list "Grimer" (list  "Poison") (list  "Ground" "Psychic"))
+(list "Muk" (list  "Poison") (list  "Ground" "Psychic"))
+(list "Shellder" (list  "Water") (list  "Electric" "Grass"))
+(list "Cloyster" (list  "Water" "Ice") (list  "Electric" "Grass" "Fighting" "Rock"))
+(list "Gastly" (list  "Ghost" "Poison") (list  "Ground" "Psychic" "Ghost" "Dark"))
+(list "Haunter" (list  "Ghost" "Poison") (list  "Ground" "Psychic" "Ghost" "Dark"))
+(list "Gengar" (list  "Ghost" "Poison") (list  "Ground" "Psychic" "Ghost" "Dark"))
+(list "Onix" (list  "Rock" "Ground") (list  "Water" "Grass" "Ice" "Fighting" "Ground" "Steel"))
+(list "Drowzee" (list  "Psychic") (list  "Bug" "Ghost" "Dark"))
+(list "Hypno" (list  "Psychic") (list  "Bug" "Ghost" "Dark"))
+(list "Krabby" (list  "Water") (list  "Electric" "Grass"))
+(list "Kingler" (list  "Water") (list  "Electric" "Grass"))
+(list "Voltorb" (list  "Electric") (list  "Ground"))
+(list "Electrode" (list  "Electric") (list  "Ground"))
+(list "Exeggcute" (list  "Grass" "Psychic") (list  "Fire" "Ice" "Poison" "Flying" "Bug" "Ghost" "Dark"))
+(list "Exeggutor" (list  "Grass" "Psychic") (list  "Fire" "Ice" "Poison" "Flying" "Bug" "Ghost" "Dark"))
+(list "Cubone" (list  "Ground") (list  "Water" "Grass" "Ice"))
+(list "Marowak" (list  "Ground") (list  "Water" "Grass" "Ice"))
+(list "Hitmonlee" (list  "Fighting") (list  "Flying" "Psychic" "Fairy"))
+(list "Hitmonchan" (list  "Fighting") (list  "Flying" "Psychic" "Fairy"))
+(list "Lickitung" (list  "Normal") (list  "Fighting"))
+(list "Koffing" (list  "Poison") (list  "Ground" "Psychic"))
+(list "Weezing" (list  "Poison") (list  "Ground" "Psychic"))
+(list "Rhyhorn" (list  "Ground" "Rock") (list  "Water" "Grass" "Ice" "Fighting" "Ground" "Steel"))
+(list "Rhydon" (list  "Ground" "Rock") (list  "Water" "Grass" "Ice" "Fighting" "Ground" "Steel"))
+(list "Chansey" (list  "Normal") (list  "Fighting"))
+(list "Tangela" (list  "Grass") (list  "Fire" "Ice" "Poison" "Flying" "Bug"))
+(list "Kangaskhan" (list  "Normal") (list  "Fighting"))
+(list "Horsea" (list  "Water") (list  "Electric" "Grass"))
+(list "Seadra" (list  "Water") (list  "Electric" "Grass"))
+(list "Goldeen" (list  "Water") (list  "Electric" "Grass"))
+(list "Seaking" (list  "Water") (list  "Electric" "Grass"))
+(list "Staryu" (list  "Water") (list  "Electric" "Grass"))
+(list "Starmie" (list  "Water" "Psychic") (list  "Electric" "Grass" "Bug" "Ghost" "Dark"))
+(list "Mr. Mime" (list  "Psychic") (list  "Bug" "Ghost" "Dark"))
+(list "Scyther" (list  "Bug" "Flying") (list  "Fire" "Electric" "Ice" "Flying" "Rock"))
+(list "Jynx" (list  "Ice" "Psychic") (list  "Fire" "Bug" "Rock" "Ghost" "Dark" "Steel"))
+(list "Electabuzz" (list  "Electric") (list  "Ground"))
+(list "Magmar" (list  "Fire") (list  "Water" "Ground" "Rock"))
+(list "Pinsir" (list  "Bug") (list  "Fire" "Flying" "Rock"))
+(list "Tauros" (list  "Normal") (list  "Fighting"))
+(list "Magikarp" (list  "Water") (list  "Electric" "Grass"))
+(list "Gyarados" (list  "Water" "Flying") (list  "Electric" "Rock"))
+(list "Lapras" (list  "Water" "Ice") (list  "Electric" "Grass" "Fighting" "Rock"))
+(list "Ditto" (list  "Normal") (list  "Fighting"))
+(list "Eevee" (list  "Normal") (list  "Fighting"))
+(list "Vaporeon" (list  "Water") (list  "Electric" "Grass"))
+(list "Jolteon" (list  "Electric") (list  "Ground"))
+(list "Flareon" (list  "Fire") (list  "Water" "Ground" "Rock"))
+(list "Porygon" (list  "Normal") (list  "Fighting"))
+(list "Omanyte" (list  "Rock" "Water") (list  "Electric" "Grass" "Fighting" "Ground"))
+(list "Omastar" (list  "Rock" "Water") (list  "Electric" "Grass" "Fighting" "Ground"))
+(list "Kabuto" (list  "Rock" "Water") (list  "Electric" "Grass" "Fighting" "Ground"))
+(list "Kabutops" (list  "Rock" "Water") (list  "Electric" "Grass" "Fighting" "Ground"))
+(list "Aerodactyl" (list  "Rock" "Flying") (list  "Water" "Electric" "Ice" "Rock" "Steel"))
+(list "Snorlax" (list  "Normal") (list  "Fighting"))
+(list "Articuno" (list  "Ice" "Flying") (list  "Fire" "Electric" "Rock" "Steel"))
+(list "Zapdos" (list  "Electric" "Flying") (list  "Ice" "Rock"))
+(list "Moltres" (list  "Fire" "Flying") (list  "Water" "Electric" "Rock"))
+(list "Dratini" (list  "Dragon") (list  "Ice" "Dragon" "Fairy"))
+(list "Dragonair" (list  "Dragon") (list  "Ice" "Dragon" "Fairy"))
+(list "Dragonite" (list  "Dragon" "Flying") (list  "Ice" "Rock" "Dragon" "Fairy"))
+(list "Mewtwo" (list  "Psychic") (list  "Bug" "Ghost" "Dark"))
+(list "Mew" (list  "Psychic") (list  "Bug" "Ghost" "Dark"))))
+
+
+
+; Implementar la siguiente función (weaker). Debe retornar una LISTA con los NOMBRES de
+; todos los pokemones mas débiles que el pokemon, cuyo nombre se pasa a través del parámetro `pokname`.
+; Se entiende que un pokemon A es más débil que un pokemon B si el listado de
+; debilidades de A contiene al menos un elemento que esté en 
+; el listado de tipos de B
+
+; El parámetro `pokedex` es una lista de pokemones. Cada pokemon es, a su vez, una lista con los siguientes elementos
+;   * Nombre
+;   * Lista de tipos
+;   * Lista de debilidades
+; Por ejemplo, para el pokemon (list "Zapdos" (list  "Electric" "Flying") (list  "Ice" "Rock")), los siguientes son sus datos:
+;   * Nombre: "Zapdos"
+;   * Tipos: "Electric" y "Flying"
+;   * Debilidades "Ice" y "Rock"
+
+; Ejemplo:
+; Si la función se llama con los parámetros "Bulbasaur" y el listado de pokemones que está al inicio de este archivo, debe retornar:
+; ("Squirtle" "Wartortle" "Blastoise" "Sandshrew" "Sandslash" "Paras" "Parasect" "Diglett" "Dugtrio" "Psyduck" "Golduck" "Poliwag" "Poliwhirl" "Poliwrath" "Geodude" "Graveler" "Golem" "Slowpoke" "Slowbro" "Seel" "Dewgong" "Shellder" "Cloyster" "Onix" "Krabby" "Kingler" "Exeggcute" "Exeggutor" "Cubone" "Marowak" "Rhyhorn" "Rhydon" "Tangela" "Horsea" "Seadra" "Goldeen" "Seaking" "Staryu" "Starmie" "Magikarp" "Lapras" "Vaporeon" "Omanyte" "Omastar" "Kabuto" "Kabutops")
+; (el orden de los elementos en la lista no es importante)
+
+; IMPORTANTE:
+; * El taller es INDIVIDUAL
+; * Debe utilizar el intérprete Biwascheme. No se permite otro intérprete
+; * Solo puede escribir código dentro de este archivo.
+; * Puede definir en este archivo todos los lambdas/funciones que Ud. necesite
+; * Todos los lambdas/funciones deben ser funciones PURAS, eso significa que
+;     no deben producir ningún tipo de efecto lateral ni utilizar ciclos. 
+; * Puede utilizar cualquier función de biwascheme, siempre y cuando cumpla con las reglas indicadas
+; ESTÁ PROHIBIDO: 
+; * Cambiar la firma de la función weaker 
+; * Escribir código en cualquier otro lugar
+; * Utilizar librerías adicionales a lo que se entrega en este proyecto
+; * Modificar valores de cualquier variable declarada en el código
+; * Utilizar ciclos
+; * Utilizar sentencias de impresión por pantalla (Todos los resultados deben ser entregados como valor de retorno de funciones)
+;
+; RECOMENDACIONES:
+; * Entienda cómo funciona la función `member` de Biwascheme
+; * Entienda cómo funciona la función `filter` de Biwascheme
+
+; Nota: Reemplace la línea (+ 2 2) por su programa
+(define weaker 
+    (lambda (pokname pokedex) 
+        (+ 2 2)   
+    )
+)
